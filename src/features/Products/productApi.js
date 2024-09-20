1a7a8e14bb9f6214ac0ea79e1efa6745c47217e6
Stranger123kr <nitesh8825kr@gmail.com>
2024-09-20 14:35:26 +0530

    some changes

--- src/features/Products/productApi.js
+++ src/features/Products/productApi.js
@@ -4,7 +4,7 @@ const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 export const FetchProductsById = async (id) => {
   try {
-    const data = await axios.get(`${backendUrl}/clothes/${id}`, {
+    const data = await axios.get(`${backendUrl}/product/${id}`, {
       // withCredentials: true,
     });
     return data.data;
@@ -18,7 +18,7 @@ export const FetchProductsById = async (id) => {
 export const FetchProducts = async (page) => {
   console.log(page);
   try {
-    const data = await axios.get(`${backendUrl}/clothes?_page=${page}`, {
+    const data = await axios.get(`${backendUrl}/product?_page=${page}`, {
       // withCredentials: true,
     });
     return data.data;
