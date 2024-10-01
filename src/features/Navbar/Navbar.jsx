85eab581842315700bd1e564ac46721831e9c8bd
“Stranger123kr” <nitesh8825kr@gmail.com>
2024-10-02 00:13:58 +0530

    do some changes

--- src/features/Navbar/Navbar.jsx
+++ src/features/Navbar/Navbar.jsx
@@ -5,7 +5,8 @@ import {
   ShoppingCartIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
-import { IoSearchOutline, IoNotifications } from "react-icons/io5";
+import { IoSearchOutline } from "react-icons/io5";
+import { LuUser, LuHeart, LuUserCheck } from "react-icons/lu";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { selectCarts } from "../Cart/CartSlice";
@@ -129,7 +130,7 @@ const Navbar = ({ Children }) => {
   return (
     <>
       <div className="min-h-full">
-        <Disclosure as="nav" className="bg-[#F8F9FA]">
+        <Disclosure as="nav" className="bg-[#f8ecd6]">
           {({ open }) => (
             <>
               <div className="mx-auto max-w-7xl px-4 sm:px-0 lg:px-0">
@@ -145,7 +146,7 @@ const Navbar = ({ Children }) => {
                       </Link>
                     </div>
                     <div className="hidden md:block">
-                      <div className="ml-10 flex items-baseline space-x-4 relative">
+                      <div className="ml-10 flex leading-[4rem] items-baseline space-x-4 relative">
                         {navigation.map((item) => (
                           <div key={item.name} className="relative group">
                             <Link
@@ -154,8 +155,8 @@ const Navbar = ({ Children }) => {
                               className={classNames(
                                 item.current
                                   ? "bg-blue-900 text-white"
-                                  : "text-[#9a9d9f] hover:bg-gray-700 hover:text-white",
-                                "rounded-md px-3 py-2 text-sm font-medium"
+                                  : "text-black hover:bg-gray-700 hover:text-white",
+                                "rounded-md px-3 py-2 text-sm font-semibold"
                               )}
                               aria-current={item.current ? "page" : undefined}
                             >
@@ -164,13 +165,13 @@ const Navbar = ({ Children }) => {
 
                             {/* Check if the item has children (dropdown) */}
                             {item.children && (
-                              <div className="absolute w-[12rem] z-50 left-0 mt-2 hidden group-hover:block bg-gray-700 rounded-lg shadow-lg">
+                              <div className="absolute w-[30rem] z-50 top-[4rem] left-0 hidden group-hover:block bg-[#f8ecd6] rounded-lg shadow-lg">
                                 <div className="py-2">
                                   {item.children.map((child) => (
                                     <Link
                                       key={child.name}
                                       to={child.link}
-                                      className="block px-4 py-2 text-sm text-gray-300 hover:bg-gray-600 hover:text-white"
+                                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-600 hover:text-white"
                                     >
                                       {child.name}
                                     </Link>
@@ -190,7 +191,7 @@ const Navbar = ({ Children }) => {
                       <div className="relative">
                         <button
                           type="button"
-                          className="relative mt-2 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                          className="relative mt-2 rounded-full bg-transparent  p-1 text-black  focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                           onClick={() => setIsOpen(true)}
                         >
                           <span className="absolute -inset-1.5" />
@@ -251,44 +252,74 @@ const Navbar = ({ Children }) => {
                           </div>
                         )}
                       </div>
-                      {/* cart*/}
+                      {/* user*/}
+                      {!!user ? (
+                        <Link to="#">
+                          <button
+                            type="button"
+                            className="relative mt-2 rounded-full bg-transparent  p-1 text-black focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                          >
+                            <span className="absolute -inset-1.5" />
+                            <LuUserCheck
+                              className="h-6 w-6"
+                              aria-hidden="true"
+                            />
+                          </button>
+                        </Link>
+                      ) : (
+                        <Link to="/login">
+                          <button
+                            type="button"
+                            className="relative mt-2 rounded-full bg-transparent  p-1 text-black focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                          >
+                            <span className="absolute -inset-1.5" />
+                            <LuUser className="h-6 w-6" aria-hidden="true" />
+                          </button>
+                          {GetAddToCart && GetAddToCart.length > 0 && (
+                            <span className="inline-flex bottom-4 right-2 relative items-center rounded-md bg-yellow-50 px-1 py-1 text-xs font-bold text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
+                              {GetAddToCart.length}
+                            </span>
+                          )}
+                        </Link>
+                      )}
+
+                      {/* wishlist*/}
                       <Link to="/cart">
-                        <button
-                          type="button"
-                          className="relative mt-2 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                        >
-                          <span className="absolute -inset-1.5" />
-                          <ShoppingCartIcon
-                            className="h-6 w-6"
-                            aria-hidden="true"
-                          />
-                        </button>
-                        {GetAddToCart && GetAddToCart.length > 0 && (
-                          <span className="inline-flex bottom-4 right-2 relative items-center rounded-md bg-yellow-50 px-1 py-1 text-xs font-bold text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
-                            {GetAddToCart.length}
-                          </span>
-                        )}
+                        <div className="relative mt-2">
+                          <button
+                            type="button"
+                            className="relative rounded-full bg-transparent p-1 text-black focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 flex items-center justify-center"
+                          >
+                            <LuHeart className="h-6 w-6" aria-hidden="true" />
+                          </button>
+                          {GetAddToCart && GetAddToCart.length > 0 && (
+                            <span className="absolute -top-1 -right-1 flex items-center justify-center h-5 w-5 rounded-full bg-orange-400 text-xs font-bold text-yellow-800">
+                              {GetAddToCart.length}
+                            </span>
+                          )}
+                        </div>
                       </Link>
-                      {/* notification*/}
-                      {/* <Link to="/">
-                        <button
-                          type="button"
-                          className="relative mt-2 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                        >
-                          <span className="absolute -inset-1.5" />
-                          <IoNotifications
-                            className="h-6 w-6"
-                            aria-hidden="true"
-                          />
-                        </button>
-                        {GetAddToCart && GetAddToCart.length > 0 && (
-                          <span className="inline-flex bottom-4 right-2 relative items-center rounded-md bg-yellow-50 px-1 py-1 text-xs font-bold text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
-                            {GetAddToCart.length}
-                          </span>
-                        )}
-                      </Link> */}
+                      {/* cart */}
+                      <Link to="/cart">
+                        <div className="relative mt-2">
+                          <button
+                            type="button"
+                            className="relative rounded-full bg-transparent p-1 text-black focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 flex items-center justify-center"
+                          >
+                            <ShoppingCartIcon
+                              className="h-6 w-6"
+                              aria-hidden="true"
+                            />
+                          </button>
+                          {GetAddToCart && GetAddToCart.length > 0 && (
+                            <span className="absolute -top-1 -right-1 flex items-center justify-center h-5 w-5 rounded-full bg-orange-400 text-xs font-bold text-yellow-800">
+                              {GetAddToCart.length}
+                            </span>
+                          )}
+                        </div>
+                      </Link>
                       {/* Profile dropdown */}
-                      <Menu as="div" className="relative">
+                      {/* <Menu as="div" className="relative">
                         {!!user ? (
                           <div>
                             <Menu.Button className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
@@ -354,7 +385,7 @@ const Navbar = ({ Children }) => {
                             ))}
                           </Menu.Items>
                         </Transition>
-                      </Menu>
+                      </Menu> */}
                       {/* <DarkMode /> */}
                     </div>
                   </div>
@@ -427,7 +458,7 @@ const Navbar = ({ Children }) => {
                     <div className="relative">
                       <button
                         type="button"
-                        className="relative mt-2 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                        className="relative mt-2 rounded-full bg-transparent  p-1 text-black  focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                         onClick={() => setIsOpen(true)}
                       >
                         <span className="absolute -inset-1.5" />
@@ -488,11 +519,53 @@ const Navbar = ({ Children }) => {
                         </div>
                       )}
                     </div>
+                    {/* {User_Profile} */}
+                    {!!user ? (
+                      <Link to="#">
+                        <button
+                          type="button"
+                          className="relative mt-2 rounded-full bg-transparent  p-1 text-black focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                        >
+                          <span className="absolute -inset-1.5" />
+                          <LuUserCheck className="h-6 w-6" aria-hidden="true" />
+                        </button>
+                      </Link>
+                    ) : (
+                      <Link to="/login">
+                        <button
+                          type="button"
+                          className="relative mt-2 rounded-full bg-transparent  p-1 text-black focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                        >
+                          <span className="absolute -inset-1.5" />
+                          <LuUser className="h-6 w-6" aria-hidden="true" />
+                        </button>
+                        {GetAddToCart && GetAddToCart.length > 0 && (
+                          <span className="inline-flex bottom-4 right-2 relative items-center rounded-md bg-yellow-50 px-1 py-1 text-xs font-bold text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
+                            {GetAddToCart.length}
+                          </span>
+                        )}
+                      </Link>
+                    )}
+                    {/* wishlist*/}
+                    <Link to="/cart">
+                      <button
+                        type="button"
+                        className="relative mt-2 rounded-full bg-transparent p-1 text-black hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                      >
+                        <span className="absolute -inset-1.5" />
+                        <LuHeart className="h-6 w-6" aria-hidden="true" />
+                        {GetAddToCart && GetAddToCart.length > 0 && (
+                          <span className="inline-flex bottom-4 left-5  absolute items-center rounded-md bg-yellow-50 px-1 py-1 text-xs font-bold text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
+                            {GetAddToCart.length}
+                          </span>
+                        )}
+                      </button>
+                    </Link>
                     {/* cart*/}
                     <Link to="/cart">
                       <button
                         type="button"
-                        className="relative mt-2 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                        className="relative mt-2 rounded-full bg-transparent p-1 text-black hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                       >
                         <span className="absolute -inset-1.5" />
                         <ShoppingCartIcon
@@ -506,56 +579,6 @@ const Navbar = ({ Children }) => {
                         )}
                       </button>
                     </Link>
-                    {/* notification*/}
-                    {/* <Link to="">
-                      <button
-                        type="button"
-                        className="relative mt-2 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                      >
-                        <span className="absolute -inset-1.5" />
-                        <IoNotifications
-                          className="h-6 w-6"
-                          aria-hidden="true"
-                        />
-                      </button>
-                      {GetAddToCart && GetAddToCart.length > 0 && (
-                        <span className="inline-flex bottom-4 right-2 relative items-center rounded-md bg-yellow-50 px-1 py-1 text-xs font-bold text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
-                          {GetAddToCart.length}
-                        </span>
-                      )}
-                    </Link> */}
-                    {!!user ? (
-                      <div className="flex-shrink-0">
-                        <img
-                          className="h-10 w-10 rounded-full"
-                          src={
-                            user?.image
-                              ? user?.image
-                              : `https://cdn2.iconfinder.com/data/icons/avatars-60/5985/13-Captain-512.png`
-                          }
-                          alt="profile_img"
-                        />
-                      </div>
-                    ) : (
-                      <Link to="/login">
-                        <button
-                          type="submit"
-                          class="flex justify-center gap-2 items-center mx-auto shadow-xl text-lg bg-gray-50 backdrop-blur-md lg:font-semibold isolation-auto border-gray-50 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-emerald-500 before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative z-10 px-4 py-2 overflow-hidden border-2 rounded-full group text-indigo-700"
-                        >
-                          Login
-                          <svg
-                            class="w-8 h-8 justify-end group-hover:rotate-90 group-hover:bg-gray-50 text-gray-50 ease-linear duration-300 rounded-full border border-gray-700 group-hover:border-none p-2 rotate-45"
-                            viewBox="0 0 16 19"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              d="M7 18C7 18.5523 7.44772 19 8 19C8.55228 19 9 18.5523 9 18H7ZM8.70711 0.292893C8.31658 -0.0976311 7.68342 -0.0976311 7.29289 0.292893L0.928932 6.65685C0.538408 7.04738 0.538408 7.68054 0.928932 8.07107C1.31946 8.46159 1.95262 8.46159 2.34315 8.07107L8 2.41421L13.6569 8.07107C14.0474 8.46159 14.6805 8.46159 15.0711 8.07107C15.4616 7.68054 15.4616 7.04738 15.0711 6.65685L8.70711 0.292893ZM9 18L9 1H7L7 18H9Z"
-                              class="fill-gray-800 group-hover:fill-gray-800"
-                            ></path>
-                          </svg>
-                        </button>
-                      </Link>
-                    )}
                     {/* <DarkMode /> */}
                   </div>
                   <div className="mt-3 space-y-1 px-2">
@@ -584,9 +607,7 @@ const Navbar = ({ Children }) => {
           </div> */}
         </header>
         <main>
-          <div className="mx-auto pt-5">
-            {Children}
-          </div>
+          <div className="mx-auto pt-5">{Children}</div>
         </main>
       </div>
     </>
