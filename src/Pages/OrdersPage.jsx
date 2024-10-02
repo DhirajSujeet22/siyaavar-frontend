fd67f07e0cfb5677b0e4692651f7565a0c2bf859
Stranger123kr <nitesh8825kr@gmail.com>
2024-10-02 17:46:49 +0530

    fix some issue

--- src/Pages/OrdersPage.jsx
+++ src/Pages/OrdersPage.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Navbar from "../features/Navbar/Navbar";
 import Footer from "../features/common/Footer";
-import Orders from "../features/User/components/orders";
+// import Orders from "../features/User/components/orders";
 import DashboardLayout from "../features/common/DashboardLayout";
 
 const OrdersPage = () => {
   return (
     <>
       <Navbar />
-      <DashboardLayout Children={<Orders />} />
+      {/* <DashboardLayout Children={<Orders />} /> */}
+      <DashboardLayout />
       <Footer />
     </>
   );
