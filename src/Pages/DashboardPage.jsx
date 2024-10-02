da90147d9167178c3da7cabd106625d5ca484025
Stranger123kr <nitesh8825kr@gmail.com>
2024-10-02 17:40:44 +0530

    fix some issue

--- src/Pages/DashboardPage.jsx
+++ src/Pages/DashboardPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../features/Navbar/Navbar";
 import Dashboard from "../features/User/components/Dashboard";
 import Footer from "../features/common/Footer";
-import Orders from "../features/User/components/orders";
+// import Orders from "../features/User/components/orders";
 
 const DashboardPage = () => {
   return (
